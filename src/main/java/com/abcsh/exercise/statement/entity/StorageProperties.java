148c9a73cc67c2b0a0216130ff5aa10927a2fe44
乘梦碧溪 <2838635235@qq.com>
2020-07-10 16:04:42 +0800

    20200710 1604 文件上传下载成功 编辑删除待完善

--- src/main/java/com/abcsh/exercise/statement/entity/StorageProperties.java
+++ src/main/java/com/abcsh/exercise/statement/entity/StorageProperties.java
@@ -2,10 +2,11 @@ package com.abcsh.exercise.statement.entity;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 
 @ConfigurationProperties("storage")
-
+@Component//*
 public class StorageProperties{
 
     private String location = "upload-dir";
